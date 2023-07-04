b72aab7d812d810295bc5912ae157024a9d96829
Crowdbotics <engineering@crowdbotics.com>
2023-07-04 11:23:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-leaf-dev-97286",
+  url: "https://royal-leaf-dev-97286.botics.co",
+  api: "https://royal-leaf-dev-97286.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
